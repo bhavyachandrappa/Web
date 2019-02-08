6273c3bd73f5469679769e8ecda9d1a55a27745c
bhavyachandrappa <35906595+bhavyachandrappa@users.noreply.github.com>
2019-02-08 13:25:42 -0400

    Add files via upload

---
+++ RequestFood.js
@@ -0,0 +1,8 @@
+﻿function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
