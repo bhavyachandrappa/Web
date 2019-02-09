00186328aae1a4c4c940c432ea4b09d879662c77
bhavyachandrappa <35906595+bhavyachandrappa@users.noreply.github.com>
2019-02-08 23:30:07 -0400

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,10 @@
+<script>
+$(function () {// JavaScript source code
+    $('[data-toggle="tooltip"]').tooltip()
+}
+)
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+    })
+</script>
