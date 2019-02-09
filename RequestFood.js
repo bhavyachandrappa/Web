4fa702f64404921595dd3d62b593efec52e5d810
bhavyachandrappa <35906595+bhavyachandrappa@users.noreply.github.com>
2019-02-08 23:28:38 -0400

    Delete RequestFood.js

--- RequestFood.js
+++
@@ -1,8 +0,0 @@
-﻿function openTab(evt, tabName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
